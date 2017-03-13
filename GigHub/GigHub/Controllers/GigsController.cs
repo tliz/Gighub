864aad93ec4af09d0f4c283270516669e186b370
Tliz <tlizzen@gmail.com>
2017-03-13 19:18:44 +0300

    Create a Gig form

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,33 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // 1. 
+        private readonly ApplicationDbContext _context;
+
+        // initialize constructor
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            // get list of genres from the database
+            // 1. DbContext
+            // 2. Create view model and set genre property
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        // Properties we need
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+        // We need a numeric value for each option in dropdown list hence int
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; } // list or collection can be used here but we wont anything using index
+    }
+}
