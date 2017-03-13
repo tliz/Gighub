aead21eeb6f084c855eb5c922614462cf8860dc3
Tliz <tlizzen@gmail.com>
2017-03-13 17:49:36 +0300

    Create Gig & Genre models

---
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        // So entity framework will know about gig class
+        public DbSet<Gig> Gigs { get; set; }
+        // Gig has reference to Genre class so EF will be able to find Genre class
+
+        // If you want to query a list of Genres, add separate dbset
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
